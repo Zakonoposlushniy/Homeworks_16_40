ba38322366576bf23cc9ab7edb2813d49db196de
Zakonoposlushniy <ertalb45@gmail.com>
2024-02-20 22:07:10 +0400

    v1

---
+++ index.py
@@ -0,0 +1,108 @@
+
+1.
+# x = int(input("enter x: "))
+# y = int(input("enter y: "))
+# A = x * y
+# print(A)
+2.
+# x = int(input("enter x: "))
+# A = x + x + x + x
+# print(A)
+3.
+# country = "Armenia"
+# print("Where are you from?", country, '!')
+# print("What is your favorite country?", country, '!')
+4.
+# x = 6.5
+# print(type(x))
+
+################################################
+1.
+# text = "python"
+# print(text[:2] * 3 + text)
+2.
+# text = "7p79"
+# print(text[0:2] *  + text)
+3.
+# text = "9"
+# print(text * 7)
+4.
+# balance = 0
+# x = float(input("insert cash 1: "))
+# y = float(input("insert cash 2: "))
+# z = float(input("insert cash 3: "))
+# balance = x + y + z 
+# print(balance)
+5.
+# number1 = 3
+# print(type (number1))
+# number2 = 42 
+# print(type (number2))
+# number3 = 2.1
+# print(type (number3))
+# number4 = -23
+# print(type (number4))
+# number5 = 2.3
+# print(type (number5))
+# number6 = 242
+# print(type (number6))
+# number7 = -43
+# print(type (number7))
+6.
+
+7.
+# P = float(input("Enter P: "))
+# Kg = P * 0.45359237
+# print(Kg)
+8.
+# m = float(input("Enter m: "))
+# km = m * 1.60934
+# print(km)
+
+################################################
+1.
+# C = float(input("Enter C: "))
+# F = (C * 1.8) + 32
+# print(F)
+2.
+# F = float(input("Enter F: "))
+# C =  9/5 * F + 32
+# print(C)
+3.
+# Minutes = int(input("Enter Minutes: "))
+# Seconds = Minutes * 60 
+# print(Seconds)
+4.
+# Days = float(input("Enter Days: "))
+# minutes = Days * 24 * 60
+# seconds = Days * 24 * 60 * 60
+# print(minutes, seconds)
+# print(type(minutes))
+# print(type(seconds))
+5.
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# a < 90
+# b < 90
+# print(a < 90, b < 90)
+6.
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# c = a + b
+# print(c)
+7.
+# x = float(input("Enter number:"))
+# print(x < 100, x > 100)
+8.
+# number = float(input("Enter a number: "))
+# print(number % 400 == 0)
+9.
+# radius = float(input("Enter the radius of the wheel: "))
+# distance = 31.4159 * radius
+# print(distance)
+10.
+# mom = int(input("Enter Moms height- "))
+# dad = int(input("Enter Dads height- "))
+# height = (mom + dad) * 0.54 - 4.5
+# print(height)
+
